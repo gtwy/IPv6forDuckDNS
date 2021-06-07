a03c66773c82c9160e1ebe4870c85fecabe3e0b6
James Watt <james@techdirect.io>
2021-06-07 14:15:32 -0400

    Fixed header information

--- autoconf-duckdns6.sh
+++ autoconf-duckdns6.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # shellcheck source=/dev/null
-# Autoconfig for DuckDNS IPv4 & IPv6 Update Script v0.3
-# By James Watt 2017-05-12
+# Autoconfig for DuckDNS IPv4 & IPv6 Update Script v1.0.3
+# By James Watt 2017-05-12 Updated 2021-06-07
 # This script is intended for hosts with an IPv4 NAT address and a public IPv6 address
 set -e
 
